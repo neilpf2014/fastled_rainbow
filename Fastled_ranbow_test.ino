c37e31d79de0e0986de71fb8a5c713e78f2bcb79
neilpf2014 <neilpf2014@gmail.com>
2016-08-15 20:13:19 -0400

    update for 5x5x5 cube

--- Fastled_ranbow_test.ino
+++ Fastled_ranbow_test.ino
@@ -1,16 +1,25 @@
 // testing fastled lib color cycle
-// also testing to see if this goes to git
-// test 2
-//  test 2
-// test 3
+// very simple program that cycles all LED's though a rainbow
+// at the same time
+// modifed to control 5 layered  LED cube
+// with each layer on separate pin
 
 #include "FastLED.h"
-CRGB leds[27];
-uint32_t pastMills = 0;
+
+#define LED_PER_LAYER 25 
+#define TOTAL_LEDS 125
+
+CRGB leds[TOTAL_LEDS];
+
+
 void setup() 
 { 
-	FastLED.addLeds<NEOPIXEL, 6>(leds, 27); 
-
+	// Need to set up all 5 layers each layer to a diff pin on the ESP8266
+	FastLED.addLeds<NEOPIXEL, 7>(leds, 0, LED_PER_LAYER);
+	FastLED.addLeds<NEOPIXEL, 1>(leds, LED_PER_LAYER, LED_PER_LAYER);
+	FastLED.addLeds<NEOPIXEL, 6>(leds, 2 * LED_PER_LAYER, LED_PER_LAYER);
+	FastLED.addLeds<NEOPIXEL, 2>(leds, 3 * LED_PER_LAYER, LED_PER_LAYER);
+	FastLED.addLeds<NEOPIXEL, 5>(leds, 4 * LED_PER_LAYER, LED_PER_LAYER);
 }
 void loop() 
 {
